d64137c395c665aaf6828289b28f252092955bf6
Ben-Slosberg <bslosbe@calstatela.edu>
2024-06-29 11:22:20 -0700

    Add files via upload

---
+++ chap_3.py
@@ -0,0 +1,27 @@
+x=5
+if x<10:
+    print('Smaller')
+if x>20:
+    print('Bigger')
+if x==5:
+    print('Is 5')
+    print('Is still 5')
+    print('Third 5')
+if x>2:
+    print('Bigger than 2')
+    print('Still bigger')
+print('Done with 2')
+
+for i in range(5):
+    print(i)
+    if i>2:
+        print('Bigger than 2')
+    print('Done with i', i)
+print('All Done')
+
+if x>2:
+    print('Bigger')
+else:
+    print('Smaller')
+
+print('All Done')  
